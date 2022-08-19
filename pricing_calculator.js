81c5a7461b28851dea79583eaa7bbf8dfbdd7484
Sam Leone <sam.leone@yale.edu>
2022-08-19 11:52:19 -0400

    Update pricing_calculator.js

--- pricing_calculator.js
+++ pricing_calculator.js
@@ -5,35 +5,40 @@ $(document).ready(function() {
   });
 
 $("#Payment").change(function() {
+    document.getElementById("Payment").value = document.getElementById("Payment").value.toLocaleString();
     calculate_price();
 });
 
 $("#full-price").change(function() {
+    document.getElementById("full-price").value = document.getElementById("full-price").value.toLocaleString();
     calculate_price();
 });
 
 $("#Term").change(function() {
+    document.getElementById("Term").value = document.getElementById("Term").value.toLocaleString();
     calculate_price();
 });
 
 $("#Interest-rate").change(function() {
+    document.getElementById("Interest-rate").value = document.getElementById("Interest-rate").value.toLocaleString();
     calculate_price();
 });
 
 $("#Salex-tax").change(function() {
+    document.getElementById("Salex-tax").value = document.getElementById("Salex-tax").value.toLocaleString();
     calculate_price();
 });
   
   
 function calculate_price(){
-    AMOUNT =  document.getElementById("full-price").value;
+    AMOUNT =  parseFloat(document.getElementById("full-price").value);
     if (AMOUNT == 0){
-        AMOUNT =  document.getElementById("full-price").placeholder;
+        AMOUNT =  parseFloat(document.getElementById("full-price").placeholder);
     }
-    down_payment = document.getElementById("Payment").value;
-    term = document.getElementById('Term').value;
-    interest_rate = document.getElementById("Interest-rate").value / 100;
-    sales_tax = document.getElementById("Salex-tax").value / 100;
+    down_payment = parseFloat(document.getElementById("Payment").value);
+    term = parseFloat(document.getElementById('Term').value);
+    interest_rate = parseFloat(document.getElementById("Interest-rate").value / 100);
+    sales_tax = parseFloat(document.getElementById("Salex-tax").value / 100);
   
     console.log(AMOUNT, down_payment, term, interest_rate, sales_tax)
       
