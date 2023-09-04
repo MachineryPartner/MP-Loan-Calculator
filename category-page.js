883e803bc68139eb95364282564c4d7427eb056e
Daniel Henrique <danihenrique@gmail.com>
2023-09-04 20:06:58 -0300

    Update Finsweet scripts

--- category-page.js
+++ category-page.js
@@ -1,131 +1,109 @@
-// Global attribute constants for Feature 2
-const dataSourceAttribute = "fs-data-source";
-const dataTargetAttribute = "fs-data-target";
-const targetParentAttribute = "fs-target-parent";
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const RICH_GEN_WRAPPER_ATTR = "fs-richgen-wrapper";
+  const DATA_SOURCE_ATTR = "fs-data-source";
+  const DATA_TARGET_ATTR = "fs-data-target";
+  const TARGET_PARENT_ATTR = "fs-target-parent";
+  const RICH_ADDED_ATTR = "fs-rich-added";
+  const POLLING_INTERVAL = 1000; // 1 second for polling interval
 
-// Feature 1: DOM Injection
-const injectSourceIntoTargetElement = (sourceElement, targetElement) => {
-  const clonedElement = sourceElement.cloneNode(true);
-  clonedElement.removeAttribute("fs-nest-element");
-  targetElement.appendChild(clonedElement);
-  targetElement.setAttribute("data-nested-inserted", "true");
-};
+  // Function to manage individual rich-gen wrappers
+  const handleRichGenWrapper = (wrapperElement) => {
+    let richAdded = false; // Flag to check if the attribute needs to be added
 
-const scanAndInjectElements = () => {
-  const elementsToBeInjected = document.querySelectorAll("[fs-nest-element]");
-  let allElementsInjected = true;
-
-  elementsToBeInjected.forEach((sourceElement) => {
-    const matchingValue = sourceElement.getAttribute("fs-nest-element");
-    const matchingTargets = document.querySelectorAll(
-      `[fs-nest-target="${matchingValue}"]:not([data-nested-inserted="true"])`
+    // Populate target elements using the corresponding source elements
+    const sourceElements = wrapperElement.querySelectorAll(
+      `[${DATA_SOURCE_ATTR}]`
     );
+    sourceElements.forEach((sourceElement) => {
+      const sourceValue = sourceElement.getAttribute(DATA_SOURCE_ATTR);
+      const textContent = sourceElement.textContent.trim();
+      const targetElements = wrapperElement.querySelectorAll(
+        `[${DATA_TARGET_ATTR}=${sourceValue}]`
+      );
 
-    if (matchingTargets.length > 0) {
-      allElementsInjected = false;
-      matchingTargets.forEach((targetElement) => {
-        injectSourceIntoTargetElement(sourceElement, targetElement);
+      targetElements.forEach((targetElement) => {
+        const parentElement = targetElement.closest(`[${TARGET_PARENT_ATTR}]`);
+        if (textContent === "") {
+          if (parentElement) {
+            parentElement.parentNode.removeChild(parentElement);
+          }
+        } else {
+          targetElement.textContent = textContent;
+          if (parentElement) {
+            parentElement.style.display = "flex";
+          }
+          richAdded = true;
+        }
       });
-    }
-  });
+    });
 
-  return allElementsInjected;
-};
-
-const executeDomInjectionAtIntervals = () => {
-  const periodicScanIntervalId = setInterval(() => {
-    const allElementsInjected = scanAndInjectElements();
-    if (allElementsInjected) {
-      clearInterval(periodicScanIntervalId);
-    }
-  }, 1000);
-};
-
-// Feature 2: Rich Generation Wrapper Handling
-const manageIndividualRichGenWrapper = (wrapperElement) => {
-  const sourceElements = wrapperElement.querySelectorAll(
-    `[${dataSourceAttribute}]`
-  );
-  sourceElements.forEach((sourceElement) => {
-    const sourceValue = sourceElement.getAttribute(dataSourceAttribute);
-    const textContent = sourceElement.textContent.trim();
-    const targetElements = wrapperElement.querySelectorAll(
-      `[${dataTargetAttribute}=${sourceValue}]`
+    // Remove target elements that do not have a corresponding source element
+    const allTargetElements = wrapperElement.querySelectorAll(
+      `[${DATA_TARGET_ATTR}]`
     );
+    allTargetElements.forEach((targetElement) => {
+      const targetValue = targetElement.getAttribute(DATA_TARGET_ATTR);
+      const correspondingSourceElement = wrapperElement.querySelector(
+        `[${DATA_SOURCE_ATTR}=${targetValue}]`
+      );
 
-    targetElements.forEach((targetElement) => {
-      const parentElement = targetElement.closest(`[${targetParentAttribute}]`);
-      if (textContent === "") {
+      if (
+        !correspondingSourceElement ||
+        correspondingSourceElement.textContent.trim() === ""
+      ) {
+        const parentElement = targetElement.closest(`[${TARGET_PARENT_ATTR}]`);
         if (parentElement) {
           parentElement.parentNode.removeChild(parentElement);
         }
-      } else {
-        targetElement.textContent = textContent;
-        if (parentElement) {
-          parentElement.style.display = "flex";
-        }
       }
     });
-  });
 
-  const allTargetElements = wrapperElement.querySelectorAll(
-    `[${dataTargetAttribute}]`
-  );
-  allTargetElements.forEach((targetElement) => {
-    const targetValue = targetElement.getAttribute(dataTargetAttribute);
-    const correspondingSourceElement = wrapperElement.querySelector(
-      `[${dataSourceAttribute}=${targetValue}]`
-    );
+    // At the end of handleRichGenWrapper function
+    wrapperElement.setAttribute(RICH_ADDED_ATTR, "true");
+  };
 
-    if (
-      !correspondingSourceElement ||
-      correspondingSourceElement.textContent.trim() === ""
-    ) {
-      const parentElement = targetElement.closest(`[${targetParentAttribute}]`);
-      if (parentElement) {
-        parentElement.parentNode.removeChild(parentElement);
-      }
-    }
-  });
-};
+  // Function to poll for new rich-gen wrapper elements
+  const pollForNewRichGenWrapperElements = () => {
+    setInterval(() => {
+      const unprocessedRichGenWrappers = document.querySelectorAll(
+        `[${RICH_GEN_WRAPPER_ATTR}]:not([${RICH_ADDED_ATTR}])`
+      );
 
-const applyRichGenWrapperToElements = () => {
-  const richGenWrapperAttribute = "fs-richgen-wrapper";
+      unprocessedRichGenWrappers.forEach(handleRichGenWrapper);
+    }, POLLING_INTERVAL);
+  };
+
+  // Get all rich-gen wrapper elements and apply the handleRichGenWrapper function to each
   const richGenWrapperElements = document.querySelectorAll(
-    `[${richGenWrapperAttribute}]`
+    `[${RICH_GEN_WRAPPER_ATTR}]`
   );
-  richGenWrapperElements.forEach(manageIndividualRichGenWrapper);
-};
+  richGenWrapperElements.forEach(handleRichGenWrapper);
 
-// Main function to initialize DOM manipulations
-const initializeDomManipulations = () => {
-  // Run DOM Injection Feature (Feature 1)
-  scanAndInjectElements();
-  executeDomInjectionAtIntervals();
+  // Start polling for new rich-gen wrapper elements
+  pollForNewRichGenWrapperElements();
 
-  // Run Rich Generation Wrapper Handling Feature (Feature 2)
-  applyRichGenWrapperToElements();
-};
+  window.fsAttributes = window.fsAttributes || [];
+  window.fsAttributes.push([
+    "cmsload",
+    (listInstances) => {
+      console.log("cmsload Successfully loaded!");
 
-// Initialize the main function when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", initializeDomManipulations);
+      // The callback passes a `listInstances` array with all the `CMSList` instances on the page.
+      const [listInstance] = listInstances;
 
-// CMS Load API
-window.fsAttributes = window.fsAttributes || [];
-window.fsAttributes.push([
-  "cmsload",
-  (listInstances) => {
-    console.log("cmsload Successfully loaded!");
+      // The `renderitems` event runs whenever the list renders items after switching pages.
+      listInstance.on("renderitems", (renderedItems) => {
+        // console.log(renderedItems);
 
-    // The callback passes a `listInstances` array with all the `CMSList` instances on the page.
-    const [listInstance] = listInstances;
+        // Fetching rich-gen wrappers again within the callback to make sure we have the latest elements.
+        const updatedRichGenWrapperElements = document.querySelectorAll(
+          `[${RICH_GEN_WRAPPER_ATTR}]`
+        );
 
-    // The `renderitems` event runs whenever the list renders items after switching pages.
-    listInstance.on("renderitems", (renderedItems) => {
-      console.log(renderedItems);
-
-      // Re-initialize your DOM manipulations
-      initializeDomManipulations();
-    });
-  },
-]);
+        // Call handleRichGenWrapper for each updated rich-gen wrapper element.
+        updatedRichGenWrapperElements.forEach(handleRichGenWrapper);
+      });
+    },
+  ]);
+});
